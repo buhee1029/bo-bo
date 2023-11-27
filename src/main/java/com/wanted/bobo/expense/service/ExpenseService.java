506d8312fcd5a23c0ce6e6a98d4b4d7de206fd43
buhee1029 <buhee1029@gmail.com>
2023-11-27 23:59:02 +0900

    Refactor: 예산으로 설정된 카테고리가 아닌 경우 지출하지 못하도록 추가

--- src/main/java/com/wanted/bobo/expense/service/ExpenseService.java
+++ src/main/java/com/wanted/bobo/expense/service/ExpenseService.java
@@ -1,5 +1,8 @@
 package com.wanted.bobo.expense.service;
 
+import com.wanted.bobo.budget.domain.Budget;
+import com.wanted.bobo.budget.domain.BudgetRepository;
+import com.wanted.bobo.category.Category;
 import com.wanted.bobo.expense.domain.Expense;
 import com.wanted.bobo.expense.domain.ExpenseRepository;
 import com.wanted.bobo.expense.dto.ExpenseFilter;
@@ -8,6 +11,7 @@ import com.wanted.bobo.expense.dto.ExpenseRequest;
 import com.wanted.bobo.expense.dto.ExpenseResponse;
 import com.wanted.bobo.expense.exception.InvalidAmountRangeException;
 import com.wanted.bobo.expense.exception.NotFoundExpenseException;
+import com.wanted.bobo.expense.exception.UnsupportedExpenseCategoryException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,6 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class ExpenseService {
 
+    private final BudgetRepository budgetRepository;
     private final ExpenseRepository expenseRepository;
 
     @Transactional(readOnly = true)
@@ -37,11 +42,15 @@ public class ExpenseService {
 
     @Transactional
     public ExpenseResponse registerExpense(Long userId, ExpenseRequest request) {
+        validateExpenseCategoryExists(userId, Category.of(request.getCategory()));
+
         return ExpenseResponse.from(expenseRepository.save(request.toEntity(userId)));
     }
 
     @Transactional
     public ExpenseResponse modifyExpense(Long userId, Long expenseId, ExpenseRequest request) {
+        validateExpenseCategoryExists(userId, Category.of(request.getCategory()));
+
         Expense expense = findExpense(expenseId);
         expense.verifyMatchUser(userId);
         expense.changeInfo(request);
@@ -65,5 +74,12 @@ public class ExpenseService {
 
     private Expense findExpense(Long id) {
         return expenseRepository.findById(id).orElseThrow(NotFoundExpenseException::new);
+    }
+
+    public void validateExpenseCategoryExists(Long userId, Category category) {
+        boolean isExpenseCategoryExists = budgetRepository.existsByUserIdAndCategory(userId, category);
+        if(!isExpenseCategoryExists) {
+            throw new UnsupportedExpenseCategoryException();
+        }
     }
 }
